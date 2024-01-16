fc9bdff5973c7854808989dfd28b95016cbcded3
Mike Kiruti <Kiruti01>
2024-01-16 13:26:59 +0300

    new pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HomePage from "@/components/HomePage";
+import InvestorPage from "@/components/landingPageTools/InvestorPage";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <HomePage />
+      <InvestorPage />
     </main>
   );
 }
