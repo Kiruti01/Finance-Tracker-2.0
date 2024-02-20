2bbef6b5a98661500c896fb2e3a0e3632a0c55cd
Michael Kiruti <mikekirutic@gmail.com>
2024-02-20 18:18:30 +0300

    next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+// import NextAuth from "next-auth";
 import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
@@ -13,45 +14,47 @@ const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
-      name: 'credentials',
+      name: "credentials",
       credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' }
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
       },
-      
+
       async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error('Invalid credentials');
-                }
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
 
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-                if (!user || !user?.password) {
-                    throw new Error('Invalid credentials');
-                }
+        if (!user || !user?.password) {
+          throw new Error("Invalid credentials");
+        }
 
-                const isCorrectPassword = await bcrypt.compare(
-                    credentials.password,
-                    user.password
-                );
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
 
-                if (!isCorrectPassword) {
-                    throw new Error('Invalid credentials');
-                }
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
 
-                return user;
-            }
-        })
-    ],
-    debug: process.env.NODE_ENV === 'development',
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
   session: {
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST, authOptions };
