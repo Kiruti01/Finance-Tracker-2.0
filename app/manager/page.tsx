3052d91624d95fe174378b61b16d466369540901
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 17:00:48 +0300

    items try

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,22 +1,23 @@
 import AddCategory from "@/components/MyFinanceComponents/AddCategory";
-// import Items from "@/components/MyFinanceComponents/Items";
+import Items from "@/components/MyFinanceComponents/Items";
 import getCategories from "@/lib/getCategories";
 
 const Page = async () => {
   const categories = await getCategories();
 
-  if (Array.isArray(categories)) {
-    if (categories.length === 0) {
-      return (
-        <div>
-          No categories found
-          <div>
-            <AddCategory />
-          </div>
+  //   if (Array.isArray(categories)) {
+  //     if (categories.length === 0) {
+  return (
+    <div>
+      No categories found
+      <div>
+        <AddCategory />
+        <div className="container mx-auto mt-6 ">
+          <Items categories={categories} />
         </div>
-      );
-    }
-  }
+      </div>
+    </div>
+  );
 
   //   return (
   //     <div className="container mx-auto mt-6 ">
