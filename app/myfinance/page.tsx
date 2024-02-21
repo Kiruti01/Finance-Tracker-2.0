629c876ccb7a9e9eec60760d6a4223067b2a31d4
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 14:47:11 +0300

    add cat2

--- app/myfinance/page.tsx
+++ app/myfinance/page.tsx
@@ -1,12 +1,31 @@
+// Myfinances.tsx
 import AddCategory from "@/components/MyFinanceComponents/AddCategory";
-// import getCategories from "@/lib/getCategories";
+import Items from "@/components/MyFinanceComponents/Items";
+import getCategories from "@/lib/getCategories";
+
+interface Categories {
+  id: string;
+  name: string;
+  TotalAmount: number;
+  userId: string;
+}
 
 const Myfinances = async () => {
-  return (
-    <div className="container mx-auto mt-6 ">
-      <AddCategory />
-    </div>
-  );
+  const categories = (await getCategories()) as Categories[];
+  console.log(categories);
+  if (Array.isArray(categories)) {
+    if (categories.length === 0) {
+      return (
+        <div>
+          No categories found
+          <div>
+            <AddCategory />
+          </div>
+        </div>
+      );
+    }
+  }
+  return <Items categories={categories} />;
 };
 
 export default Myfinances;
