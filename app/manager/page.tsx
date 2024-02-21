4f9b2619acb23e0eceded154fc28de851827ee82
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 14:54:30 +0300

    add cat funct

---
+++ app/manager/page.tsx
@@ -0,0 +1,23 @@
+// Myfinances.tsx
+import AddCategory from "@/components/MyFinanceComponents/AddCategory";
+import Items from "@/components/MyFinanceComponents/Items";
+import getCategories from "@/lib/getCategories";
+
+const Myfinances = async () => {
+  const categories = await getCategories();
+  if (Array.isArray(categories)) {
+    if (categories.length === 0) {
+      return (
+        <div>
+          No categories found
+          <div>
+            <AddCategory />
+          </div>
+        </div>
+      );
+    }
+  }
+  return <Items categories={categories} />;
+};
+
+export default Myfinances;
