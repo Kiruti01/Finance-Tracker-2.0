6118f7822c2d0b966a64c0732c586079f9d34ca1
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 13:33:59 +0300

    try

--- app/transactions/[transactionId]/page.tsx
+++ app/transactions/[transactionId]/page.tsx
@@ -1,48 +1,51 @@
-"use client"
+"use client";
 
-import TransactionCard from '@/components/TransactionCards/TransactionCard';
-import getCategories from '@/lib/getCategories';
-import axios from 'axios';
-import { useParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import TransactionCard from "@/components/TransactionCards/TransactionCard";
+import getCategories from "@/lib/getCategories";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 interface Transaction {
-    id: string;
-    investmentType: string;
-    investmentAmount: number;
-    rateOfInterest: number | null;
-    timePeriod: string | null;
-    return: number | null;
-    totalReturns: number | null;
-    access: boolean;
-    userId: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  investmentType: string;
+  investmentAmount: number;
+  rateOfInterest: number | null;
+  timePeriod: string | null;
+  return: number | null;
+  totalReturns: number | null;
+  access: boolean;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const page = () => {
-    const params = useParams();
-    const [transaction, setTransaction] = useState<Transaction | null>(null);
+const Page = () => {
+  const params = useParams();
+  const [transaction, setTransaction] = useState<Transaction | null>(null);
 
-    useEffect(() => {
-        const transactionfetch = async () => {
-            try {
-                const response = await axios.get(`/api/transaction?id=${params.transactionId}`);
-                console.log(response)
-                setTransaction(response.data.data)
-            } catch (error) {
-                console.log("Error")
-            }
-        }
-        transactionfetch()
-    }, []);
+  useEffect(() => {
+    const transactionfetch = async () => {
+      try {
+        const response = await axios.get(
+          `/api/transaction?id=${params.transactionId}`
+        );
+        console.log(response);
+        setTransaction(response.data.data);
+      } catch (error) {
+        console.log("Error");
+      }
+    };
+    transactionfetch();
+  }, []);
 
-    return (
-        <div>
-            {transaction && <TransactionCard transactionDetails={transaction} categories={null}/>}
-        </div>
-    );
+  return (
+    <div>
+      {transaction && (
+        <TransactionCard transactionDetails={transaction} categories={null} />
+      )}
+    </div>
+  );
+};
 
-}
-
-export default page
+export default Page;
