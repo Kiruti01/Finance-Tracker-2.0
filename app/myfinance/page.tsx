c6719fe6a749bdded393d27ecc44f388fd8e5be2
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 13:53:45 +0300

    fix page

--- app/myfinance/page.tsx
+++ app/myfinance/page.tsx
@@ -1,29 +1,7 @@
-"use client";
-import AddCategory from "@/components/MyFinanceComponents/AddCategory";
-import Items from "@/components/MyFinanceComponents/Items";
-import getCategories from "@/lib/getCategories";
+import React from "react";
 
-const Page = async () => {
-  const categories = await getCategories();
+function page() {
+  return <div>My finance</div>;
+}
 
-  if (Array.isArray(categories)) {
-    if (categories.length === 0) {
-      return (
-        <div>
-          No categories found
-          <div>
-            <AddCategory />
-          </div>
-        </div>
-      );
-    }
-  }
-
-  return (
-    <div className="container mx-auto mt-6 ">
-      <Items categories={categories} />
-    </div>
-  );
-};
-
-export default Page;
+export default page;
