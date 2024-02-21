da7e7e3c4ad52a018cfb6566fb852d2e57524dac
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 14:09:10 +0300

    add cat funct

--- app/myfinance/page.tsx
+++ app/myfinance/page.tsx
@@ -1,15 +1,29 @@
 import AddCategory from "@/components/MyFinanceComponents/AddCategory";
+import Items from "@/components/MyFinanceComponents/Items";
+import getCategories from "@/lib/getCategories";
 import React from "react";
 
-function Page() {
+const Page = async () => {
+  const categories = await getCategories();
+
+  if (Array.isArray(categories)) {
+    if (categories.length === 0) {
+      return (
+        <div>
+          No categories found
+          <div>
+            <AddCategory />
+          </div>
+        </div>
+      );
+    }
+  }
+
   return (
-    <div>
-      No categories found
-      <div>
-        <AddCategory />
-      </div>
+    <div className="container mx-auto mt-6 ">
+      <Items categories={categories} />
     </div>
   );
-}
+};
 
 export default Page;
