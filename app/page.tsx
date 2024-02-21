374da3e11d61cb7213af2c1154ca608880acdf2c
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 16:08:54 +0300

    manager component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/LandingPage/Footer";
 import HomePage from "@/components/LandingPage/HomePage";
 import SharePage from "@/components/LandingPage/SharePage";
 import StartCalculating from "@/components/LandingPage/StartCalculating";
-import Manager from "@/components/manager/manager";
+// import Manager from "@/components/manager/manager";
 // import InvestorPage from "@/components/landingPageTools/InvestorPage";
 
 // import Image from "next/image";
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <HomePage />
-      <Manager />
+      {/* <Manager /> */}
       <StartCalculating />
       {/* <InvestorPage /> */}
       <SharePage />
