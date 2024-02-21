55b99d7994645a210b451dc6db17a5980f51b773
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 12:25:06 +0300

    Update options.ts

--- lib/options.ts
+++ lib/options.ts
@@ -1,5 +1,8 @@
 import { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { db as prisma } from "@/lib/db";
+
 
 const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
