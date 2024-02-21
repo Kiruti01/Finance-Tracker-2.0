29308d5e8e9b22bd9d4313b3a8aa6fe89d2be731
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 17:30:46 +0300

    try again aga

--- components/manager/manager.tsx
+++ components/manager/manager.tsx
@@ -1,28 +1,28 @@
-// import AddCategory from "@/components/MyFinanceComponents/AddCategory";
-import Items from "@/components/MyFinanceComponents/Items";
+import AddCategory from "@/components/MyFinanceComponents/AddCategory";
+// import Items from "@/components/MyFinanceComponents/Items";
 import getCategories from "@/lib/getCategories";
 
 const Page = async () => {
   const categories = await getCategories();
 
-  // if (Array.isArray(categories)) {
-  //   if (categories.length === 0) {
-  //     return (
-  //       <div>
-  //         No categories found
-  //         <div>
-  //           <AddCategory />
-  //         </div>
-  //       </div>
-  //     );
-  //   }
-  // }
+  if (Array.isArray(categories)) {
+    if (categories.length === 0) {
+      return (
+        <div>
+          No categories found
+          <div>
+            <AddCategory />
+          </div>
+        </div>
+      );
+    }
+  }
 
-  return (
-    <div className="container mx-auto mt-6 ">
-      <Items categories={categories} />
-    </div>
-  );
+  // return (
+  //   <div className="container mx-auto mt-6 ">
+  //     <Items categories={categories} />
+  //   </div>
+  // );
 };
 
 export default Page;
