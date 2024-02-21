6b71c321bd9aab812718c17054dbf9c31f9e3504
Michael Kiruti <mikekirutic@gmail.com>
2024-02-21 19:53:10 +0300

    21st Feb last commit

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,8 +1,37 @@
 // import Manager from "@/components/manager/manager";
+import AddCategory from "@/components/MyFinanceComponents/AddCategory";
+import Items from "@/components/MyFinanceComponents/Items";
+import getCategories from "@/lib/getCategories";
 import React from "react";
 
-function Page() {
-  return <div>{/* <Manager /> */}</div>;
+interface Categories {
+  id: string;
+  name: string;
+  TotalAmount: number;
+  userId: string;
 }
+const Page = async () => {
+  const Categories = (await getCategories()) as Categories[];
+
+  console.log(Categories);
+  if (Array.isArray(Categories)) {
+    if (Categories.length === 0) {
+      return (
+        <div>
+          No categories found
+          <div>
+            <AddCategory />
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="container mx-auto mt-6 ">
+      <Items categories={Categories} />
+    </div>
+  );
+};
 
 export default Page;
